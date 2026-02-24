92705d93adee0a495b226433c44bad437049e1bd
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2026-02-24 17:27:58 +0100

    retry 2 times every second

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,8 +1,8 @@
 pub mod dto;
 pub mod queue;
 
-pub const VISIBILITY_TIMEOUT_SECONDS: i32 = 2;
-pub const MAX_RETRIES: i32 = 5;
+pub const VISIBILITY_TIMEOUT_SECONDS: i32 = 1;
+pub const MAX_RETRIES: i32 = 2;
 pub const PG_URL: &str = "postgres://postgres:postgres@0.0.0.0:5432";
 pub const KAFKA_BOOTSTRAP_SERVERS: &str = "localhost:9092";
 pub const WHATSAPP_MSG_QUEUE: &str = "whatsapp_messages";
